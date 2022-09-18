e2ac850c619439fc97c31fd04aeb68209e6eff4b
bagsang-yun <ygvbhy@naver.com>
2022-09-18 13:59:47 +0900

    애니메이션을 직접 만들어본 코드 말이 애니메이션이지 0.01초 마다 렌더를 시켜 위치값만 바꿔준 안좋은 코드

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`
+
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const [intervalId, setIntervalId] = useState(null)
+  const moveUp = () => {
+    const id = setInterval(() => setY(prev => prev + 1), 10)
+    setIntervalId(id)
+  }
+  useEffect(() => {
+    if(y === 200) {
+      clearInterval(intervalId)
+    }
+  }, [y, intervalId])
+  return <Container>
+    <Box onPress={moveUp} style={{
+      transform: [{translateY: y}]
+    }} />
+  </Container>;
 }
 
 
