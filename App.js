7dde91f8f9e4f6bd23f7da1ab9ce1f695f074504
bagsang-yun <ygvbhy@naver.com>
2022-09-18 14:24:01 +0900

    애니메이션을 만드는 기초적인 단계

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import styled from "styled-components/native";
+import {Animated} from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -11,26 +12,33 @@ const Box = styled.TouchableOpacity`
   width: 200px;
   height: 200px;
 `
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+// 애니메이션과 styled 를 같이 사용 할 때 선언 하는 방법은 두가지
+// 직접 한줄에 선언하는 방법
+// const AnimatedBox = styled(Animated.createAnimatedComponent(TouchableOpacity))
+// 위의 방법 처럼 하면 react-native 의 TouchableOpacity 를 같이 선언 해야 함
+// 이 방법이 싫다면 밑의 방법
+// const AnimatedBox = Animated.createAnimatedComponent(Box);
+// 위의 방법은 react-native 의 TouchableOpacity 를 직접 선언 하지 않고 styled 의 선언된 부분을 애니메이션으로 사용이 가능
 
 export default function App() {
-  const [y, setY] = useState(0);
-  const [intervalId, setIntervalId] = useState(null)
+
+  // 1. useState 를 사용하지 않음
+  // 2. Y 의 Value 를 직접 정하지 않음
+  // 3. 애니메이션의 규칙이 있음 View 를 직접 쓰지 않고 Animated.View 이렇게 선언 해야함
+
+  const Y = new Animated.Value(0)
   const moveUp = () => {
-    // 말이 애니메이션이지만 아님.
-    // 0.01초마다 반복시켜 y축의 값만 바꿔준 아주 안좋은 애니메이션
-    // 0.01초마다 렌더를 시키기 때문에 만약 이렇게 할거면 그냥 때려 치는게 맞다
-    const id = setInterval(() => setY(prev => prev + 1), 10)
-    setIntervalId(id)
+
   }
-  useEffect(() => {
-    if(y === 200) {
-      clearInterval(intervalId)
-    }
-  }, [y, intervalId])
   return <Container>
-    <Box onPress={moveUp} style={{
-      transform: [{translateY: y}]
-    }} />
+    <AnimatedBox
+      onPress={moveUp}
+      style={{
+        transform: [{translateY: Y}]
+      }}
+    />
   </Container>;
 }
 
