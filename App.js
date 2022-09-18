2db4b35540213276a60bc2e4b78cde684a2abcae
bagsang-yun <ygvbhy@naver.com>
2022-09-18 14:56:18 +0900

    애니메이션의 효과 두가지

--- App.js
+++ App.js
@@ -7,38 +7,80 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `
-const Box = styled.TouchableOpacity`
+// TouchableOpacity 는 애니메이션이 잘 안먹음
+// View 에 관련 된건 다 잘 먹기 때문에 그걸로 바꿔줌
+// 근데 View 는 onPress 를 잃기 때문에 밖에 다시 감싸줘야함
+const TimingBox = styled.View`
   background-color: tomato;
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
 `
-const AnimatedBox = Animated.createAnimatedComponent(Box);
+const AnimatedBox = Animated.createAnimatedComponent(TimingBox);
 
-// 애니메이션과 styled 를 같이 사용 할 때 선언 하는 방법은 두가지
-// 직접 한줄에 선언하는 방법
-// const AnimatedBox = styled(Animated.createAnimatedComponent(TouchableOpacity))
-// 위의 방법 처럼 하면 react-native 의 TouchableOpacity 를 같이 선언 해야 함
-// 이 방법이 싫다면 밑의 방법
-// const AnimatedBox = Animated.createAnimatedComponent(Box);
-// 위의 방법은 react-native 의 TouchableOpacity 를 직접 선언 하지 않고 styled 의 선언된 부분을 애니메이션으로 사용이 가능
+// 두가지의 예시가 나오므로 따로 해서 만들어 봄
+const SpringBox = styled.View`
+  background-color: blue;
+  width: 100px;
+  height: 100px;
+`
+const AnimatedSpringBox = Animated.createAnimatedComponent(SpringBox)
+
+// 선언하기 싫어서 따로 만듬
+const Touch = styled.TouchableOpacity``
 
 export default function App() {
-
-  // 1. useState 를 사용하지 않음
-  // 2. Y 의 Value 를 직접 정하지 않음
-  // 3. 애니메이션의 규칙이 있음 View 를 직접 쓰지 않고 Animated.View 이렇게 선언 해야함
-
   const Y = new Animated.Value(0)
+  const X = new Animated.Value(0)
+  // Animated.Value 를 수정하는 방법은 3가지
+  // https://reactnative.dev/docs/0.66/animated 자세한 내용
+  // 가장 자주 사용하는건 timing() 임
   const moveUp = () => {
-
+    Animated.timing(Y, {
+      // Y 로 지정된 부분부터 어디까지 가는지에 대한 선언
+      toValue: 200,
+      // 항상 true 로 둠
+      // 애니메이션에 대한 모든것을 native 쪽으로 보냄. 애니메이션이 시작 전에
+      useNativeDriver: true
+    }).start()
+    // 시작을 해줘야 애니메이션이 실행됨
+    // start 안에 또 지정이 가능
   }
+
+  const moveDown = () => {
+    // 통통 튀는 효과
+    Animated.spring(X, {
+      toValue: -100,
+      // 얼마나 튈지 설정
+      // bounciness: 15,
+      // bounciness 와 speed 를 같이 쓸 수 있음
+
+      friction: 10,
+      tension: 800, // 탄성 ? 느낌 높을 수록 많이 튐 ㅋㅋ 느낌이 선언 한 숫자 만큼 튕구고 멈추는 듯 함
+      // tension 과 frictions 을 같이 쓸 수 있음
+      useNativeDriver: true
+    }).start()
+  }
+  // 여기서 Y 가 바뀌는걸 보면 딱 한번 만 나옴 처음 렌더 할때만
+  console.log(Y)
+  // Y 가 바뀌는걸 보고 싶으면 이벤트를 줘야 함
+  Y.addListener(() => console.log(Y))
+  // 이렇게 하면 Y 의 변하는 과정이 나옴
+
   return <Container>
-    <AnimatedBox
-      onPress={moveUp}
-      style={{
-        transform: [{translateY: Y}]
-      }}
-    />
+    <Touch onPress={moveUp}>
+      <AnimatedBox
+        style={{
+          transform: [{translateY: Y}]
+        }}
+      />
+    </Touch>
+    <Touch onPress={moveDown}>
+      <AnimatedSpringBox
+        style={{
+          transform: [{translateX: X}]
+        }}
+      />
+    </Touch>
   </Container>;
 }
 
