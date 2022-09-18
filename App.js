eb6d6d43e45f1c947b52e3591d04db5d2ef557d2
bagsang-yun <ygvbhy@naver.com>
2022-09-18 17:53:45 +0900

    애니메이션 드래그 기능

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React, {useRef} from 'react';
 import styled from "styled-components/native";
-import {Animated, Dimensions} from "react-native";
+import {
+  Animated,
+  Dimensions,
+  PanResponder
+} from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -14,54 +18,19 @@ const TimingBox = styled.View`
 `
 const AnimatedBox = Animated.createAnimatedComponent(TimingBox);
 
-const Touch = styled.Pressable``
-
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 // https://reactnative.dev/docs/0.66/animated
 // 애니메이션에 대한 자세한 내용 정리
 // 버전에 맞게 확인 하면 됨
 export default function App() {
-  // 위치를 바꾼 이유 : 애니메이션은 정상 작동 하는데 가운데가 디폴트라 끊기면서 중앙으로 갔다가 다시 시작됨
-  const POSITION = useRef(new Animated.ValueXY({x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100})).current
-  // 모양이 나오는 위치 값 정의 변수의 이름대로 왼상 우상 왼하 우하
-  const topLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100
-    },
-    useNativeDriver: false,
-  })
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100
-    },
-    useNativeDriver: false,
-  })
-  const topRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100
-    },
-    useNativeDriver: false,
-  })
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100
-    },
-    useNativeDriver: false,
-  })
-  const moveUp = () => {
-    // 무한 반복
-    Animated.loop(
-        // 애니메이션이 시작 되면 선언 한 순서대로 진행 됨
-        Animated.sequence([
-          bottomLeft, bottomRight, topRight, topLeft
-        ])
-    ).start()
-  }
+  const POSITION = useRef(
+      new Animated.ValueXY(
+          {
+            x: 0,
+            y: 0
+          })
+  ).current
 
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-250, 250],
@@ -73,21 +42,38 @@ export default function App() {
     outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
   })
 
-  return <Container>
-    <Touch onPress={moveUp}>
-      <AnimatedBox
-        style={{
-          borderRadius,
-          backgroundColor: bgColor,
-          transform: [
-              // 아래의 함수를 사용 하면 transform 에 대한 값들이 나오게 설정 되어 있음
-              // js 문법 사용 해서 ... 함수 해두면 값들이 선언됨
-              ...POSITION.getTranslateTransform(),
-          ]
-        }}
-      />
-    </Touch>
-  </Container>;
+  // 터치로 드래그 할 수 있는 함수 생성
+  // 웹에선 마우스로 드래그 기능
+  const panResponder = useRef(
+      PanResponder.create({
+        // 어떠한 터치도 사용하겠다는 선언
+        onStartShouldSetPanResponder: () => true,
+        // 몇개의 손가락으로 터치 했는지 터치한 위치가 어딘지 까지 값이 나옴 dx, dy
+        onPanResponderMove: (_, {dx, dy}) => {
+            // 해당 view 의 위치를 재설정 해주는 명령
+          POSITION.setValue({
+            x: dx,
+            y: dy
+          })
+        }
+      })
+  ).current;
+
+  return (
+    <Container>
+        <AnimatedBox
+          /*  펜 핸들러를 사용하기 위해 선언 */
+          {...panResponder.panHandlers}
+          style={{
+            borderRadius,
+            backgroundColor: bgColor,
+            transform: [
+                ...POSITION.getTranslateTransform(),
+            ]
+          }}
+        />
+    </Container>
+  )
 }
 
 
