143c3ee710aefb0d6a34b985fcba65db4f082907
bagsang-yun <ygvbhy@naver.com>
2022-09-25 13:55:29 +0900

    카드 애니메이션 만들기 2

--- App.js
+++ App.js
@@ -26,35 +26,43 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 
 export default function App() {
     const scale = useRef(new Animated.Value(1)).current;
+    const onPressIn = () =>
+        Animated.spring(scale, {toValue: 0.95, useNativeDriver: true}).start()
+    const onPressOut = () =>
+        Animated.spring(scale, {toValue: 1, useNativeDriver: true}).start()
+    const position = useRef(new Animated.Value(0)).current
+    const rotation = position.interpolate({
+        inputRange: [-250, 250],
+        outputRange: ["-15deg", "15deg"],
+        // 위에 설정한 값을 넘어 갈 때 행동할것을 정해줌
+        extrapolate: 'clamp'
+    })
+
     const panResponder = useRef(PanResponder.create({
         onStartShouldSetPanResponder: () => true,
-        // 애니메이션 시작
         onPanResponderGrant: () => onPressIn(),
-        // 애니메이션 종료시 줄엇던 크기를 늘려주면서 위치 초기화 0 번 자리에
-        onPanResponderRelease: () => {
+        onPanResponderRelease: (_, {dx}) => {
+            // dx 의 위치 값에 따라 할것을 정해줌
+            if( dx < -320){
+                Animated.spring(position, {toValue: -400, useNativeDriver: true}).start()
+            } else if (dx > 320) {
+                Animated.spring(position, {toValue: 400, useNativeDriver: true}).start()
+            } else {
+                Animated.spring(position, {toValue : 0, useNativeDriver: true}).start()
+            }
             onPressOut()
-            Animated.spring(position, {toValue : 0, useNativeDriver: true}).start()
         },
-        // 애니메이션이 움직일때 x 값만 바꿔줌
         onPanResponderMove: (_, {dx}) => {
             position.setValue(dx)
         },
     })).current;
-    // 눌렀을떄 반응할 애니메이션
-    const onPressIn = () =>
-        Animated.spring(scale, {toValue: 0.95, useNativeDriver: true}).start()
-    // 터치가 끝났을때 반응 할 애니메이션
-    const onPressOut = () =>
-        Animated.spring(scale, {toValue: 1, useNativeDriver: true}).start()
-    // 초기 위치
-    const position = useRef(new Animated.Value(0)).current
 
     return (
         <Container>
             <Card
                 {...panResponder.panHandlers}
                 style={{
-                transform: [{scale}, {translateX: position}]
+                transform: [{scale}, {translateX: position}, {rotateZ: rotation}]
                 }}
             >
                 <Ionicons name="pizza" color="#192a56" size={98} />
